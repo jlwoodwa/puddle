9a0e4c9bceeccda5c9c2f930d285966ce30350fe
june <jlwoodwa@caltech.edu>
2020-01-16 11:51:03 -0800

    expanding on the mathematical parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,35 @@
 module Main where
 
-import Control.Applicative hiding (some)
+import Control.Applicative hiding (some, (<|>))
 import Data.Either.Combinators
 import Data.Void
 import Text.Parsec
 import Text.Parsec.Char
+import Data.Maybe
 
 type Parser = Parsec String ()
 
-data Math = Numb Double | Expr Op Math Math deriving (Show)
+data Math = Numb Double | Expr Math Op Math deriving (Show)
 data Op = Add | Mult | Sub | Div deriving (Show)
+
+toOp :: Char -> Op
+toOp = fromJust . flip lookup [('+', Add), ('*', Mult), ('-', Sub), ('/', Div)]
 
 main = do
   let x = "2.0"
-  print $ parse (expr <* eof) "" x
+  print $ parse (math <* eof) "" x
+
+numb :: Parser Math
+numb = Numb . read <$> double
+
+op :: Parser Op
+op = toOp <$> oneOf "+*-/"
 
 expr :: Parser Math
-expr = Numb . read <$> double
+expr = undefined
+
+math :: Parser Math
+math = numb <|> expr
 
 double :: Parser String
 double = perhap minus id (:) <*> many1 digit <**> perhap decimal id (flip (++))
