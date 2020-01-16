478a1225da297c359cd6c32a25ebb086015b83a7
june <jlwoodwa@caltech.edu>
2020-01-16 10:51:59 -0800

    cleaned up and added type signatures

--- src/Main.hs
+++ src/Main.hs
@@ -10,13 +10,16 @@ type Parser = Parsec Void String
 main = parseTest double "-2.0"
 
 double :: Parser String
-double = perhap minus id (:) <*> some digitChar <**> perhap decimal id (flip (++))
+double =
+  perhap minus id (:) <*> some digitChar <**> perhap decimal id (flip (++))
 
+minus :: Parser (Maybe Char)
 minus = optional $ char '-'
 
 decimal :: Parser (Maybe String)
 decimal = optional $ char '.' <:> some digitChar
 
+-- Utility functions
 (<:>) = liftA2 (:)
 
 perhap parser def op = maybe def op <$> parser
