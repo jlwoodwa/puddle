e1d80ddaf41415334d69e2571269fd550c202538
june <jlwoodwa@caltech.edu>
2020-01-16 10:13:34 -0800

    playing around with parsers

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,22 @@
 module Main where
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+import Control.Applicative hiding (some)
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+type Parser = Parsec Void String
+
+main = parseTest double "-2"
+
+double :: Parser String
+double = minus ?:> some digitChar
+
+minus = optional (char '-')
+
+Nothing ?: y = y
+Just x ?: y = x : y
+
+(?:>) = liftA2 (?:)
+
+perhap parser op = maybe id op parser
