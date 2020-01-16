cf2c09722105c5152a681ae4b94e1663051f2dc2
june <jlwoodwa@caltech.edu>
2020-01-16 10:22:53 -0800

    cleaned up source

--- src/Main.hs
+++ src/Main.hs
@@ -10,14 +10,8 @@ type Parser = Parsec Void String
 main = parseTest double "-2"
 
 double :: Parser String
---double = minus ?:> some digitChar
 double = perhap minus (:) <*> some digitChar
 
-minus = optional (char '-')
-
---Nothing ?: y = y
---Just x ?: y = x : y
-
---(?:>) = liftA2 (?:)
+minus = optional $ char '-'
 
 perhap parser op = maybe id op <$> parser
