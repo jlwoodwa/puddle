9357643fc85b9166a396d3d9bdb5719662046804
june <jlwoodwa@caltech.edu>
2020-01-17 11:39:09 -0800

    cleaning up some

--- src/Main.hs
+++ src/Main.hs
@@ -9,32 +9,32 @@ import Text.Parsec
 import Text.Parsec.Char
 
 main = do
-	let x = "if 2 = 3 then 1 else 0"
-	print $ parse (code <* eof) "" x
---   forever $ do
---     x <- getLine
---     print . eval . handler $ parse (math <* eof) "" x
+  let x = "if 2 = 3 then 1 else 0"
+  print $ parse (code <* eof) "" x
 
 type Parser = Parsec String ()
 
 -- code interpreter
-
-data Code = Numerical Math | Boolean Bool | Decision Code Code Code
+data Code
+  = Numerical Math
+  | Boolean Bool
+  | Decision Code Code Code
 
 evalC (Numerical x) = x
 evalC (Boolean x) = x
-evalC (Decision (Boolean b) x y) = if b then x else y
+evalC (Decision (Boolean b) x y) =
+  if b
+    then evalC x
+    else evalC y
 evalC (Decision _ _ _) = error "invalid decision structure"
 
 -- math interpreter
-
 evalM :: Math -> Double
 evalM (Numb x) = x
 evalM (Expr a Add b) = evalM a + evalM b
 evalM (Expr a Mult b) = evalM a * evalM b
 evalM (Expr a Sub b) = evalM a - evalM b
 evalM (Expr a Div b) = evalM a / evaMl b
-
 
 data Math
   = Numb Double
@@ -51,7 +51,6 @@ data Op
 toOp :: Char -> Op
 toOp = fromJust . flip lookup [('+', Add), ('*', Mult), ('-', Sub), ('/', Div)]
 
-
 numb :: Parser Math
 numb = Numb . read <$> double
 
@@ -59,10 +58,14 @@ op :: Parser Op
 op = toOp <$> oneOf "+*-/"
 
 expr1 :: Parser Math
-expr1 = wrapSpaces $ try (Expr <$> expr2 <*> fmap toOp (oneOf "+-") <*> expr1) <|> expr2
+expr1 =
+  wrapSpaces $
+  try (Expr <$> expr2 <*> fmap toOp (oneOf "+-") <*> expr1) <|> expr2
 
 expr2 :: Parser Math
-expr2 = wrapSpaces $ try (Expr <$> expr3 <*> fmap toOp (oneOf "/*") <*> expr2) <|> expr3
+expr2 =
+  wrapSpaces $
+  try (Expr <$> expr3 <*> fmap toOp (oneOf "/*") <*> expr2) <|> expr3
 
 expr3 :: Parser Math
 expr3 = wrapSpaces $ try (char '(' *> expr1 <* char ')') <|> numb
